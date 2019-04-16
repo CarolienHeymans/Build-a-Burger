54608c246835b49c86775ce3262d09093cd3021e
Carolien <heymanscarolien@gmail.com>
2019-04-16 14:38:03 +0200

    Updates

---
+++ precache-manifest.0d953ae622c401cd87982f626347ec6a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9517f771de624a2c1791",
+    "url": "/Build-a-Burger/static/js/runtime~main.cd9b86c9.js"
+  },
+  {
+    "revision": "d4ab864f6dc3a3aac721",
+    "url": "/Build-a-Burger/static/js/main.266653a3.chunk.js"
+  },
+  {
+    "revision": "dca97451db0c87f3e195",
+    "url": "/Build-a-Burger/static/js/2.7eae1618.chunk.js"
+  },
+  {
+    "revision": "d4ab864f6dc3a3aac721",
+    "url": "/Build-a-Burger/static/css/main.0e132543.chunk.css"
+  },
+  {
+    "revision": "dca97451db0c87f3e195",
+    "url": "/Build-a-Burger/static/css/2.a730db2b.chunk.css"
+  },
+  {
+    "revision": "24e45700ee2d051deab74e7eb40db209",
+    "url": "/Build-a-Burger/index.html"
+  }
+];
